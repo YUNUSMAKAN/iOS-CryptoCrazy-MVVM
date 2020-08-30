fd7928223badde51e0194e17e18d2614c393df96
Yunus MAKAN <makanyunus@gmail.com>
2020-08-31 02:01:37 +0300

    Model created!

---
+++ CryptoCrazyMVVM/Model/CryptoCurrency.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCurrency.swift
+//  CryptoCrazyMVVM
+//
+//  Created by MAKAN on 30.08.2020.
+//  Copyright © 2020 YUNUS MAKAN. All rights reserved.
+//
+
+import Foundation
+
+
+struct CryptoCurrency : Decodable {
+    
+    let currency : String
+    let price : String
+    
+}
