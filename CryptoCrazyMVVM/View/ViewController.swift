d9f96615d31f8213a109ba069b8449c087f2916c
Yunus MAKAN <makanyunus@gmail.com>
2020-09-06 17:05:27 +0300

    Showed data!

--- CryptoCrazyMVVM/View/ViewController.swift
+++ CryptoCrazyMVVM/View/ViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController , UITableViewDataSource, UITableViewDelegate {
+    
     @IBOutlet weak var tableView: UITableView!
+    
+    private var cryptoListViewModel : CryptoListViewModel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,24 +21,40 @@ class ViewController: UIViewController , UITableViewDataSource, UITableViewDeleg
         tableView.delegate = self
         tableView.dataSource = self
         
+        getData()
+    }
+    
+    func getData() {
+        
         let url = URL(string: "https://raw.githubusercontent.com/atilsamancioglu/K21-JSONDataSet/master/crypto.json")!
         
         Webservice().downloadCurrencies(url: url) { (cryptos) in
             if let cryptos = cryptos {  //Optional olmaktan cikardik.
                 
             
+                self.cryptoListViewModel = CryptoListViewModel(cryptoCurrencyList: cryptos)
+                
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
             }
         }
     }
     
     
-    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
-    }
+        
+        return self.cryptoListViewModel == nil ? 0 : self.cryptoListViewModel.numberOfRowsInSection() //nil olup olmadigini kontrol ettik.
+        }
+
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "CryptoCell", for: indexPath) as! CryptoTableViewCell
+        
+        let cryptoViewModel = self.cryptoListViewModel.cryptoAtIndex(indexPath.row)
+        
+        cell.priceText.text = cryptoViewModel.price
+        cell.currencyText.text = cryptoViewModel.name
         
         return cell
         
